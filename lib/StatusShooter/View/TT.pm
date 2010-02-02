b6819ffe27fc2881710bc36c6551d612c972d789
John SJ Anderson <genehack@genehack.org>
2010-02-02 05:10:07 -0500

    add TT view

---
+++ lib/StatusShooter/View/TT.pm
@@ -0,0 +1,33 @@
+package StatusShooter::View::TT;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+StatusShooter::View::TT - TT View for StatusShooter
+
+=head1 DESCRIPTION
+
+TT View for StatusShooter.
+
+=head1 SEE ALSO
+
+L<StatusShooter>
+
+=head1 AUTHOR
+
+genehack
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
