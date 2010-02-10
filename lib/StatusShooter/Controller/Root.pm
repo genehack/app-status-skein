6bdd46aa56c3fd6b8683caccaee0ffeddf59a24e
John SJ Anderson <genehack@genehack.org>
2010-02-10 10:25:34 -0500

    clean up logic in form validation/processing

--- lib/StatusShooter/Controller/Root.pm
+++ lib/StatusShooter/Controller/Root.pm
@@ -19,32 +19,31 @@ sub index :Path :Args(0) {
   $c->stash(
     template => 'index.tt' ,
     form     => $self->form ,
-    message  => 'Not Valid' ,
   );
 
-  return unless $self->form->process( params => $c->req->parameters );
+  if( $self->form->process( params => $c->req->parameters )) {
+    my $result = $self->form->value;
 
-  my $result = $self->form->value;
+    my %services = map { $_ => 1 } @{ $result->{services} };
 
-  my %services = map { $_ => 1 } @{ $result->{services} };
+    ### FIXME all the interaction with services should be handled by model classes...
 
-  my $body;
+    if ( $services{blog} ) {
+      my $post = _post_on_blog( $result );
 
-  if ( $services{blog} ) {
-    my $post = _post_on_blog( $result );
+      _post_note_to_facebook( $post , $result )           if ( $services{facebook} );
+      _post_blog_post_title_to_twitter( $post , $result ) if ( $services{twitter}  );
+    }
+    else {
+      _post_status_to_facebook( $result ) if ( $services{facebook} );
+      _post_status_to_twitter( $result )  if ( $services{twitter}  );
+    }
 
-    _post_note_to_facebook( $post , $result )           if ( $services{facebook} );
-    _post_blog_post_title_to_twitter( $post , $result ) if ( $services{twitter}  );
+    $c->stash(
+      message => "Posted" ,
+      form    => StatusShooter::Form::Update->new ,
+    );
   }
-  else {
-    _post_status_to_facebook( $result ) if ( $services{facebook} );
-    _post_status_to_twitter( $result )  if ( $services{twitter}  );
-  }
-
-  $c->stash(
-    message => "Posted" ,
-    form    => StatusShooter::Form::Update->new ,
-  );
 }
 
 sub default :Path {
