6315d8da96dd91c59ecdbd4d456eabf5ce24cf5c
John SJ Anderson <genehack@genehack.org>
2010-02-12 21:54:00 -0500

    switch Model classes over to wrapping StatusShooter::Client classes

--- lib/StatusShooter/Model/Facebook.pm
+++ lib/StatusShooter/Model/Facebook.pm
@@ -3,11 +3,6 @@ use strict;
 use warnings;
 use base 'Catalyst::Model::Adaptor';
 
-__PACKAGE__->config( class => 'WWW::Facebook::API' );
-
-sub mangle_arguments {
-  my( $self , $args ) = @_;
-  return %$args;
-}
+__PACKAGE__->config( class => 'StatusShooter::Client::Facebook' );
 
 1;
