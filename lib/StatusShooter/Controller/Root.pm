b5ec763f0dd3346833735fb226af455bd27a15ea
John SJ Anderson <genehack@genehack.org>
2010-02-13 10:06:31 -0500

    process twitter input, handling twitter_in_reply_to field correctly

--- lib/StatusShooter/Controller/Root.pm
+++ lib/StatusShooter/Controller/Root.pm
@@ -58,8 +58,13 @@ sub index :Path :Args(0) {
       }
 
       if ( $services{twitter} ) {
-        eval { $c->model('Twitter')->update( $result->{status} ) };
-        if ( $@ ) { return $c->stash( message => $@ ) }
+        my $args = { status => $result->{status} };
+        $args->{in_reply_to_status_id} = $result->{twitter_in_reply_to}
+          if $result->{twitter_in_reply_to};
+        eval { $c->model('Twitter')->update( $args ) };
+        if ( $@ ) {
+          return $c->stash( message => $@ );
+        }
       }
     }
 
