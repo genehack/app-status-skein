c91ed60abf44b224cc5ee5e677114f6a690e94b2
John SJ Anderson <genehack@genehack.org>
2010-02-12 21:53:39 -0500

    initial commit of Client classes
    Moose classes that serve as clients for accessing Twitter and Facebook, respectively.

---
+++ lib/StatusShooter/Client/Twitter.pm
@@ -0,0 +1,39 @@
+use MooseX::Declare;
+
+class StatusShooter::Client::Twitter extends StatusShooter::Client {
+  use Net::Twitter;
+  use StatusShooter::Post::Twitter;
+
+  has '_client' => (
+    is         => 'ro' ,
+    isa        => 'Net::Twitter' ,
+    lazy_build => 1 ,
+  );
+
+  has traits              => ( is => 'ro' , isa => 'ArrayRef' , required => 1 );
+  has access_token        => ( is => 'ro' , isa => 'Str'      , required => 1 );
+  has access_token_secret => ( is => 'ro' , isa => 'Str'      , required => 1 );
+  has consumer_secret     => ( is => 'ro' , isa => 'Str'      , required => 1 );
+  has consumer_key        => ( is => 'ro' , isa => 'Str'      , required => 1 );
+
+  method _build__client {
+    return Net::Twitter->new(
+      traits              => $self->traits ,
+      access_token        => $self->access_token ,
+      access_token_secret => $self->access_token_secret ,
+      consumer_secret     => $self->consumer_secret ,
+      consumer_key        => $self->consumer_key ,
+    );
+  }
+
+  method get_posts {
+    my $posts;
+    eval { $posts = $self->_client->friends_timeline };
+    die $@ if $@;
+
+    $posts = [ map { StatusShooter::Post::Twitter->new({ post => $_ }) } @$posts ];
+
+    return $posts;
+  }
+}
+
