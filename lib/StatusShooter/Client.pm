c0b4cc24acc7a7d0938f6ddaf30ed67ac079aea3
John SJ Anderson <genehack@genehack.org>
2010-02-13 12:24:13 -0500

    add get_post method to Client base class

--- lib/StatusShooter/Client.pm
+++ lib/StatusShooter/Client.pm
@@ -5,5 +5,14 @@ class StatusShooter::Client {
     isa => 'Str' ,
   );
 
+  method get_post ( $id ) {
+    my $post;
+    eval { $post = $self->show_status( $id ) };
+    die $@ if $@;
+
+    my $post_class = $self->post_class;
+    return $post_class->new({ post => $post });
+  }
+
   method post_class { return sprintf "StatusShooter::Post::%s" , $self->type }
 }
