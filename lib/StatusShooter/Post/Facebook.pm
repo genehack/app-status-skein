bd4f4630c6b286f21f65318d807876da90b10f67
John SJ Anderson <genehack@genehack.org>
2010-02-12 23:05:27 -0500

    rework Post classes
    factor attrs out into base
    add BUILD method to rewrite text attr to hotlink things

--- lib/StatusShooter/Post/Facebook.pm
+++ lib/StatusShooter/Post/Facebook.pm
@@ -1,4 +1,5 @@
 use MooseX::Declare;
+
 class StatusShooter::Post::Facebook extends StatusShooter::Post {
   use DateTime;
 
@@ -10,7 +11,20 @@ class StatusShooter::Post::Facebook extends StatusShooter::Post {
     required => 1 ,
   );
 
-  method author { return $self->profile->{name} }
-  method date   { return DateTime->from_epoch( epoch => $self->post->{created_time} ) }
-  method text   { return $self->post->{message} }
+  # lazy builder for 'date' attr, declared in base class
+  method _build_date {
+    my $dt = DateTime->from_epoch( epoch => $self->post->{created_time} );
+    $dt->set_time_zone( 'local' );
+    return $dt;
+  }
+
+  # lazy builder for 'text' attr, declared in base class
+  method _build_text { return $self->post->{message} }
+
+  method author      { return $self->profile->{name} }
+  method avatar_src  { return $self->profile->{pic_square} }
+  method permalink   { return $self->post->{permalink} }
+  method user_desc   { return $self->author }
+  method user_handle { return $self->profile->{name} }
+  method user_url    { return $self->profile->{url} }
 }
