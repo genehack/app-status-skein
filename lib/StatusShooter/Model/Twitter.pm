d07d3d82f2fde788dc5213541b60b0648b0d3c91
John SJ Anderson <genehack@genehack.org>
2010-02-12 21:54:00 -0500

    switch Model classes over to wrapping StatusShooter::Client classes

--- lib/StatusShooter/Model/Twitter.pm
+++ lib/StatusShooter/Model/Twitter.pm
@@ -3,11 +3,6 @@ use strict;
 use warnings;
 use base 'Catalyst::Model::Adaptor';
 
-__PACKAGE__->config( class => 'Net::Twitter' );
-
-sub mangle_arguments {
-  my( $self , $args ) = @_;
-  return %$args;
-}
+__PACKAGE__->config( class => 'StatusShooter::Client::Twitter' );
 
 1;
