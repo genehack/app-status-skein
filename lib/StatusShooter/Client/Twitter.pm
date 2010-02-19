91f4ebf42df3856c4d11ffd4dc8723c4ff5c6f6a
John SJ Anderson <genehack@genehack.org>
2010-02-19 14:55:26 -0500

    switch over to user/pass for Twitter

--- lib/StatusShooter/Client/Twitter.pm
+++ lib/StatusShooter/Client/Twitter.pm
@@ -17,21 +17,17 @@ class StatusShooter::Client::Twitter extends StatusShooter::Client {
     lazy_build => 1 ,
   );
 
-  has traits              => ( is => 'ro' , isa => 'ArrayRef' , required => 1 );
-  has access_token        => ( is => 'ro' , isa => 'Str'      , required => 1 );
-  has access_token_secret => ( is => 'ro' , isa => 'Str'      , required => 1 );
-  has consumer_secret     => ( is => 'ro' , isa => 'Str'      , required => 1 );
-  has consumer_key        => ( is => 'ro' , isa => 'Str'      , required => 1 );
+  has traits   => ( is => 'ro' , isa => 'ArrayRef' , required => 1 );
+  has username => ( is => 'ro' , isa => 'Str'      , required => 1 );
+  has password => ( is => 'ro' , isa => 'Str'      , required => 1 );
 
   has '+type' => ( default => 'Twitter' );
 
   method _build__client {
     return Net::Twitter->new(
-      traits              => $self->traits ,
-      access_token        => $self->access_token ,
-      access_token_secret => $self->access_token_secret ,
-      consumer_secret     => $self->consumer_secret ,
-      consumer_key        => $self->consumer_key ,
+      traits   => $self->traits ,
+      username => $self->username ,
+      password => $self->password ,
     );
   }
 }
