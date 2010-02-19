66cde4116f304699dbb21d0381ebb808c9f3c627
John SJ Anderson <genehack@genehack.org>
2010-02-19 14:55:46 -0500

    switch to using C::M::Factory

--- lib/StatusShooter/Model/Twitter.pm
+++ lib/StatusShooter/Model/Twitter.pm
@@ -1,7 +1,7 @@
 package StatusShooter::Model::Twitter;
 use strict;
 use warnings;
-use base 'Catalyst::Model::Adaptor';
+use base 'Catalyst::Model::Factory';
 
 __PACKAGE__->config( class => 'StatusShooter::Client::Twitter' );
 
