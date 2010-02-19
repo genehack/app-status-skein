f6f03f50bd9673f7f0c222052293b99dbcc403f3
John SJ Anderson <genehack@genehack.org>
2010-02-19 17:54:55 -0500

    remove service-specific client model wrappers. replace with generic CLI client model wrapper.

--- lib/App/StatusSkein/Web/Model/Identica.pm
+++
@@ -1,8 +0,0 @@
-package App::StatusSkein::Web::Model::Identica;
-use strict;
-use warnings;
-use base 'Catalyst::Model::Adaptor';
-
-__PACKAGE__->config( class => 'App::StatusSkein::CLI::Client::Identica' );
-
-1;
--- lib/App/StatusSkein/Web/Model/Twitter.pm
+++
@@ -1,8 +0,0 @@
-package App::StatusSkein::Web::Model::Twitter;
-use strict;
-use warnings;
-use base 'Catalyst::Model::Adaptor';
-
-__PACKAGE__->config( class => 'App::StatusSkein::CLI::Client::Twitter' );
-
-1;
