ad468e2b8664d520cf7920233aea35f9f1a00052
John SJ Anderson <genehack@genehack.org>
2010-02-19 20:14:17 -0500

    load CLI config in Model wrapper

--- lib/App/StatusSkein/Web/Model/CLI.pm
+++ lib/App/StatusSkein/Web/Model/CLI.pm
@@ -5,4 +5,8 @@ use base 'Catalyst::Model::Adaptor';
 
 __PACKAGE__->config( class => 'App::StatusSkein::CLI' );
 
+my $config = App::StatusSkein::Web->path_to( 'app_statusskein_cli.yaml' );
+
+__PACKAGE__->config( args => { config_file => $config } );
+
 1;
