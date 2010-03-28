7a3031c8e5a29532f7caa8b5435c5003c022c306
John SJ Anderson <john.jacobsanderson@nih.gov>
2010-03-28 18:58:14 -0400

    Move the config for Model::CLI into the app config file.
    this gets around the path_to() method not really being available in
    the model class. I was calling it as a package method on the app
    class, but that breaks the use_ok() in the model test...

--- lib/App/StatusSkein/Web/Model/CLI.pm
+++ lib/App/StatusSkein/Web/Model/CLI.pm
@@ -1,11 +1,4 @@
 package App::StatusSkein::Web::Model::CLI;
 use base 'Catalyst::Model::Adaptor';
 
-my $config = App::StatusSkein::Web->path_to( 'app_statusskein_cli.yaml' );
-
-__PACKAGE__->config(
-  args  => { config_file => $config } ,
-  class => 'App::StatusSkein::CLI' ,
-);
-
 1;
